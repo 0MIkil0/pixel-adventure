b47ddffbd7c2e038785b44521d363bf73fcc30cc
Михаил Александорович Хоботов <mikil@MacBook-Air-Mihail-2.local>
2026-04-26 14:02:46 +0300

    сохранение очков если сцена перезагружается и вывод их в ui

--- Assets/Pixel Adventure 1/Assets/Scripts/Score.cs
+++ Assets/Pixel Adventure 1/Assets/Scripts/Score.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
    public int playerScore;
-
+   private TMP_Text _scoreText;
+    
+   void Start()
+   {
+      _scoreText = GetComponent<TMP_Text>();
+   }
+   void Update()
+   {
+      _scoreText.text = $"Очки: {playerScore}";
+   }
    public void AddScore()
    {
       playerScore++;
